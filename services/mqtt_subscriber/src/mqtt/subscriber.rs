73c26a68830c8f647c575b14880c140de28da91e
Shahram Barai <shahram.barai@gmail.com>
2025-03-25 22:56:31 +0200

    Add connection status management to MQTT Subscriber

--- services/mqtt_subscriber/src/mqtt/subscriber.rs
+++ services/mqtt_subscriber/src/mqtt/subscriber.rs
@@ -3,6 +3,7 @@
 use log::{error, info};
 use rumqttc::{AsyncClient, EventLoop, MqttOptions, QoS};
 use std::collections::HashSet;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -11,6 +12,7 @@ pub struct MqttSubscriber {
     client: AsyncClient,
     topics: Arc<RwLock<HashSet<String>>>,
     mqtt_qos: QoS,
+    is_connected: AtomicBool,
 }
 
 impl MqttSubscriber {
@@ -25,11 +27,22 @@ impl MqttSubscriber {
             client,
             topics: Arc::new(RwLock::new(HashSet::new())),
             mqtt_qos,
+            is_connected: AtomicBool::new(false),
         };
 
         info!("MQTT client created");
 
         (subscriber, event_loop)
+    }
+
+    /// Check if the MQTT client is connected
+    pub fn is_connected(&self) -> bool {
+        self.is_connected.load(Ordering::Relaxed)
+    }
+
+    /// Update the connection status
+    pub fn update_connection_status(&self, status: bool) {
+        self.is_connected.store(status, Ordering::Relaxed);
     }
 
     /// Subscribe to a topic
@@ -90,5 +103,21 @@ impl MqttSubscriber {
     pub async fn get_topics(&self) -> Vec<String> {
         let topics_read = self.topics.read().await;
         topics_read.iter().cloned().collect()
+    }
+
+    /// Resubscribe to all topics
+    pub async fn resubscribe_to_topics(&self) {
+        let topics_to_resubscribe = self.get_topics().await;
+
+        if topics_to_resubscribe.is_empty() {
+            return;
+        }
+
+        for topic in topics_to_resubscribe {
+            match self.subscribe(&topic).await {
+                Ok(_) => info!("Resubscribed to topic: {}", topic),
+                Err(e) => error!("Failed to resubscribe to {}: {:?}", topic, e),
+            }
+        }
     }
 }
