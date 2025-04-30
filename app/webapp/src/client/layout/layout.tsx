a0d9549fcdb6ca99f21f99383dce5a23512eb5bb
Shahram Barai <shahram.barai@gmail.com>
2025-04-30 17:18:59 +0300

    Add Switch component and integrate theme toggle in AppLayout

--- app/webapp/src/client/layout/layout.tsx
+++ app/webapp/src/client/layout/layout.tsx
@@ -1,19 +1,32 @@
 import Head from "next/head";
 import "../../styles/globals.css";
+import { Switch } from "../components/basics/switch";
+import { useState } from "react";
 
 export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [theme, setTheme] = useState<"light" | "dark" | "system">("system");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === "light" ? "dark" : prevTheme === "dark" ? "system" : "light"
+    );
+  };
+
   return (
-    <div className="">
+    <div className={theme}>
       <Head>
         <title>Webapp</title>
         <meta name="description" content="Webapp" />
         <link rel="icon" href="/globe.svg" />
       </Head>
-      <main className="flex min-h-screen bg-background text-foreground">
+      <main className="flex flex-col min-h-screen bg-background text-foreground">
+        <div className="flex px-6 py-2 w-full items-center justify-end">
+          <Switch checked={theme === "dark"} onCheckedChange={toggleTheme} />
+        </div>
         {children}
       </main>
     </div>
