ae645a89aee9228bc0ba5d56724477ac3f8917f3
Shahram Barai <shahram.barai@gmail.com>
2025-05-05 22:31:15 +0300

    Refactor PropertyPanel to improve node handling and update mechanism
    - Converted PropertyPanel to a memoized component for performance optimization.
    - Updated the component to use `useReactFlow` for better integration with the flow state.
    - Simplified node update handling by utilizing a callback function.
    - Enhanced rendering logic to display appropriate editors based on the selected node type.
    - Removed deprecated props and streamlined the component structure for clarity.

--- app/webapp/src/client/components/complex/PropertyPanel/index.tsx
+++ app/webapp/src/client/components/complex/PropertyPanel/index.tsx
@@ -1,75 +1,89 @@
 import { Button } from "@/client/components/basics/Button";
 import { cn } from "@/client/utils";
 import { BookmarkIcon } from "@heroicons/react/16/solid";
-import { Node } from "@xyflow/react";
+import { Node, useReactFlow } from "@xyflow/react";
 import KafkaSource from "./KafkaSource";
 import Filter from "./Filter";
 import { KafkaSourceFormValues } from "./KafkaSource/schemas";
+import { memo, useCallback } from "react";
+import { FilterFormValues } from "./Filter/schemas";
 
-export * from "./KafkaSource/index";
+const PropertyPanel = memo(
+  ({ className, node }: { className?: string; node: Node | null }) => {
+    const reactFlow = useReactFlow();
 
-export const PropertyPanel = ({
-  className,
-  selectedNode,
-  onNodeUpdate,
-}: {
-  className?: string;
-  selectedNode: Node | null;
-  onNodeUpdate: (nodeId: string, data: KafkaSourceFormValues) => void;
-}) => {
-  // Early return if no node is selected
-  if (!selectedNode) {
+    // Node update handler
+    const handleNodeUpdate = useCallback(
+      (nodeId: string, newData: KafkaSourceFormValues | FilterFormValues) => {
+        reactFlow.updateNodeData(nodeId, newData);
+      },
+      [reactFlow]
+    );
+
+    // Early return if no node is selected
+    if (!node) {
+      return (
+        <div className={cn("flex flex-col", className)}>
+          <div className="bg-surface flex justify-between items-center py-2 px-4">
+            <span className="text-base font-medium text-muted-foreground">
+              No node selected
+            </span>
+          </div>
+        </div>
+      );
+    }
+
+    const getSourceNodeId = () => {
+      const sourceNode = reactFlow
+        .getEdges()
+        .find((edge) => edge.target === node.id);
+      return reactFlow.getNodes().find((node) => node.id === sourceNode?.source)
+        ?.id;
+    };
+
+    // Determine which editor to show based on node type
+    const renderEditor = () => {
+      switch (node.data.label) {
+        case "Kafka Source":
+          return (
+            <KafkaSource
+              key={node.id}
+              data={node.data as KafkaSourceFormValues}
+              setData={(data: KafkaSourceFormValues) => {
+                handleNodeUpdate(node.id, data);
+              }}
+            />
+          );
+        case "Filter":
+          return (
+            <Filter
+              key={node.id}
+              nodeId={node.id}
+              sourceNodeId={getSourceNodeId()}
+            />
+          );
+        default:
+          return <div>Unknown node: {node.id}</div>;
+      }
+    };
+
     return (
       <div className={cn("flex flex-col", className)}>
         <div className="bg-surface flex justify-between items-center py-2 px-4">
-          <span className="text-base font-medium text-muted-foreground">
-            No node selected
+          <span className="text-base font-medium text-foreground">
+            {`${node.data.label} (${node.id})`}
           </span>
+          <Button variant="primary">
+            <BookmarkIcon className="size-4 mr-1" />
+            Save
+          </Button>
         </div>
+        <div className="flex-1 overflow-y-auto">{renderEditor()}</div>
       </div>
     );
   }
+);
 
-  // Handle node data updates
-  const handleUpdate = async (data: KafkaSourceFormValues) => {
-    if (selectedNode) {
-      // Update the node data
-      onNodeUpdate(selectedNode.id, data);
-      return true;
-    }
-    return false;
-  };
+PropertyPanel.displayName = "PropertyPanel";
 
-  // Determine which editor to show based on node type
-  const renderEditor = () => {
-    switch (selectedNode.data.label) {
-      case "Kafka Source":
-        return (
-          <KafkaSource
-            key={selectedNode.id}
-            data={selectedNode.data as KafkaSourceFormValues}
-            setData={handleUpdate}
-          />
-        );
-      case "Filter":
-        return <Filter key={selectedNode.id} />;
-      default:
-        return <div>Unknown node: {selectedNode.id}</div>;
-    }
-  };
-
-  return (
-    <div className={cn("flex flex-col", className)}>
-      <div className="bg-surface flex justify-between items-center py-2 px-4">
-        <span className="text-base font-medium text-foreground">
-          {`${selectedNode.data.label} (${selectedNode.id})`}
-        </span>
-        <Button variant="primary">
-          <BookmarkIcon className="size-4 mr-1" />
-          Save
-        </Button>
-      </div>
-      <div className="flex-1 overflow-y-auto">{renderEditor()}</div>
-    </div>
-  );
-};
+export default PropertyPanel;
