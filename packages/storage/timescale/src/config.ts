fa3205b2576a7c68ca6714cbc4736025fb5e5f36
Shahram Barai <shahram.barai@gmail.com>
2026-03-06 11:44:18 +0000

    Add a config file for environment variables

---
+++ packages/storage/timescale/src/config.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import { logger } from "@spine/shared";
+
+dotenv.config({ path: ["../../../.env", ".env"] });
+
+let inputVariable: string | undefined;
+let NODE_ENV: string;
+let DATABASE_URL: string;
+
+// Node environment
+inputVariable = process.env.NODE_ENV;
+if (!inputVariable) {
+    logger.warn("NODE_ENV is not set in the environment variables. Defaulting to 'prod'.");
+    inputVariable = "prod";
+}
+NODE_ENV = inputVariable;
+
+// Database URL
+inputVariable = process.env.DATABASE_URL_TIMESCALE;
+if (!inputVariable) {
+  throw new Error("DATABASE_URL_TIMESCALE is not set in the environment variables. Please check the .env file.");
+}
+DATABASE_URL = inputVariable;
+
+export { DATABASE_URL, NODE_ENV };
