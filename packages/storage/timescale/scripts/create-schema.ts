dfd7846b800c80010ffb7dae90c06750656e2778
Shahram Barai <shahram.barai@gmail.com>
2026-03-25 12:37:31 +0000

    Add TimescaleDB connection using environment variables in the schema creation script.

--- packages/storage/timescale/scripts/create-schema.ts
+++ packages/storage/timescale/scripts/create-schema.ts
@@ -1,6 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "../../../../.env" });
+
+if (!process.env.DATABASE_URL_TIMESCALE) {
+    throw new Error("DATABASE_URL_TIMESCALE is not defined");
+}
+
+import { initTimescaleStorage } from "../src/db/connection";
 import { createSchema } from "../src/db/schema";
 
 console.log("🔃​ Creating schema...\n");
+
+await initTimescaleStorage({
+    databaseUrl: `postgresql://${process.env.DATABASE_URL_TIMESCALE}`,
+});
 
 const { success, error } = await createSchema();
 
