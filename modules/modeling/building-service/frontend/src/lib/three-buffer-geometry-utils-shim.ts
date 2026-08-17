f644f317780383180e1a51457a9653be0465de84
Shahram Barai <shahram.barai@gmail.com>
2026-08-18 00:05:49 +0300

    Remove bulding service frontend components and utilities

--- modules/modeling/building-service/frontend/src/lib/three-buffer-geometry-utils-shim.ts
+++
@@ -1,6 +0,0 @@
-export * from "three/examples/jsm/utils/BufferGeometryUtils.js";
-
-import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils.js";
-
-// web-ifc-three expects mergeGeometries export name.
-export const mergeGeometries = mergeBufferGeometries;
